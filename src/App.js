ef74f0e0ce26bada83f17f28f0ab09278c40a83e
Jorge <jorgediazok@gmail.com>
2020-10-22 19:07:37 -0300

    created Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
