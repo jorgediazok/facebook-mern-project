0d0742c08f437673d68d197eec02acfddeea9832
Jorge <jorgediazok@gmail.com>
2020-10-22 14:45:31 -0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyAz2vpA0HFIKWjxRF0VeGXNGH2If7pqOqE',
+  authDomain: 'facebook-mern-project.firebaseapp.com',
+  databaseURL: 'https://facebook-mern-project.firebaseio.com',
+  projectId: 'facebook-mern-project',
+  storageBucket: 'facebook-mern-project.appspot.com',
+  messagingSenderId: '521860035283',
+  appId: '1:521860035283:web:6d24fd3257109a623c5a26',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
