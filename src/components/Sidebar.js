f275909352197a9d6291035d19ff90fb81fc66c7
Jorge <jorgediazok@gmail.com>
2020-10-23 02:21:40 -0300

    added icons of sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import SidebarRow from '../components/SidebarRow';
+import { ExpandMoreOutlined } from '@material-ui/icons';
 import '../styles/Sidebar.css';
 
 function Sidebar() {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <SidebarRow
+        src="https://avatars1.githubusercontent.com/u/47897689?s=460&u=0bb395d093ce35cd806306d0c62b1d86aebfbaa1&v=4"
+        title="Jorge DeV"
+      />
+      <SidebarRow
+        Icon={LocalHospitalIcon}
+        title="COVID-19 Information Center"
+      />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="More..." />
+    </div>
+  );
 }
 
 export default Sidebar;
