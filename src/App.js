0dd70ad41056bc744a7372161ede3f31ede63c22
Jorge <jorgediazok@gmail.com>
2020-10-23 02:48:34 -0300

    created feed and storyreel components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className="app__body">
         <Sidebar />
+        <Feed />
       </div>
     </div>
   );
---
+++ src/components/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StoryReel from '../components/StoryReel';
+import '../styles/Feed.css';
+
+function Feed() {
+  return (
+    <div className="feed">
+      <StoryReel />
+    </div>
+  );
+}
+
+export default Feed;
