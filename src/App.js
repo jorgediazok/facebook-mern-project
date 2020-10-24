2225490b8e492b74c32b31287819ae2d3b7e20ee
Jorge <jorgediazok@gmail.com>
2020-10-24 01:47:43 -0300

    login page done

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Widgets from './components/Widgets';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <React.Fragment>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </React.Fragment>
+      )}
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import '../styles/Login.css';
+
+function Login() {
+  const signIn = () => {};
+
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
+          alt="login-logo"
+        />
+        <img
+          src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg"
+          alt="login-logo-2"
+        />
+      </div>
+      <Button type="submit" onClick={signIn}>
+        Sign In
+      </Button>
+    </div>
+  );
+}
+
+export default Login;
